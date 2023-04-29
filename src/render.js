82b83346df4b3766b98dc66baa275a3e6cadedf2
chenyuhao <cccchenyuhao@gmail.com>
2023-04-30 05:24:31 +0800

    histogram

--- src/render.js
+++ src/render.js
@@ -1,8 +1,21 @@
 import * as d3 from "d3"
-import drawDonutChart from "./donut-charts"
-import drawStackedBars from "./drawStackedBars"
+import drawHistogram from "./drawHistogram"
+const getRandomSalary = (salary) => {
+  const lowerLimit = +salary.slice(1, salary.indexOf(" -")).replace(",", "")
+  const upperLimit = +salary.slice(salary.indexOf(" $") + 2).replace(",", "")
+
+  return Math.floor(Math.random() * (upperLimit - lowerLimit) + lowerLimit)
+}
+
 export default async function render() {
-  const data = await d3.csv("data/data.csv", d3.autoType)
-  drawDonutChart(data)
-  drawStackedBars(data)
+  const data = await d3.csv("data/data.csv", (d) => {
+    if (d.pay_annual_USD !== "$240,000 or more") {
+      return {
+        ...d,
+        salary: getRandomSalary(d.pay_annual_USD),
+      }
+    }
+  })
+
+  drawHistogram(data)
 }
